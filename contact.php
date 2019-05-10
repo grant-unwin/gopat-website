0ca2589f1c31dd8c1b805ea9dfb7aeba876541c8
thesaloncorp <35975940+thesaloncorp@users.noreply.github.com>
2019-05-10 20:11:24 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'GOPAT <website@gopat.co.uk>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <yourEmail@example.com>';
+$sendTo = 'Sales <richard@gopat.co.uk>';
 
 // subject of the email
 $subject = 'New message from contact form';
